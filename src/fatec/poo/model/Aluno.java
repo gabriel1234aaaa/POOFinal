1d6f513550ae96c24b185ea68f918f46f04c2e6d
0030481711046 <0030481711046@LTI6104.lti.fatecso.edu.br>
2018-11-06 12:05:03 -0200

    Trabalho 2 de POO

---
+++ src/fatec/poo/model/Aluno.java
@@ -0,0 +1,24 @@
+package fatec.poo.model;
+
+public class Aluno extends Pessoa {
+    private String Escolaridade;
+    private Matricula matricula;
+    
+    public Aluno(String Nome, String CPF) {
+        super(Nome, CPF);
+    }
+
+    public Matricula getAluno_matricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Matricula matricula) {
+        this.matricula = matricula;
+        matricula.setAluno(this);
+    }
+    
+    public void setEscolaridade(String Escolaridade) {
+        this.Escolaridade = Escolaridade;
+    }
+    
+}
